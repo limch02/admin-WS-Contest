8fb246d7dac88cd7983c2536b20db4839640d9f1
임찬혁 <dlacksgur7@naver.com>
2025-11-01 20:48:43 +0900

    main

---
+++ src/main/java/com/admincontest/AdminContestApplication.java
@@ -0,0 +1,13 @@
+package com.admincontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminContestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminContestApplication.class, args);
+	}
+
+}
